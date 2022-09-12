0f4b41bb661bbebcc02bde15a32885cbcc64470d
dhimas putra <hello.dhimasputra@gmail.com>
2022-09-12 13:44:12 +0800

    change colors

--- component/Navbar.js
+++ component/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar() {
             <div className="nav-item-container">
                 <Link href='/#'><a className='nav-item'>Home</a></Link>
                 <Link href='/work'><a className='nav-item'>Works</a></Link>
-                <Link href='/waiting'><a className='nav-item'>Resources</a></Link>
+                <Link href='/waiting'><a className='nav-item'>Learn</a></Link>
                 <Link href='/contact'><a className='nav-item'>Contact</a></Link>
             </div>
         </div>
