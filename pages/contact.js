043afa1a5af4a1f7f2e70212c29dad04409c9420
dhimas putra <hello.dhimasputra@gmail.com>
2022-09-16 17:44:36 +0800

    add rel

--- pages/contact.js
+++ pages/contact.js
@@ -7,24 +7,24 @@ export default function contact() {
     <Layout>
       <section className="contact-hero">
         <div className="content-container">
-        <a className="email" href="mailto:hello@dhimasputra.com" target='_blank'><h1>hello@dhimasputra.com</h1></a>
+        <a className="email" href="mailto:hello@dhimasputra.com" target='_blank' rel="noopener noreferrer"><h1>hello@dhimasputra.com</h1></a>
         <p className="desc text-center">Let’s connect your brain with me, and build a crazy product.</p>
         <div className="contact-container">
             <p>Follow my socials</p>
            <div className="contact-btn">
-              <a href="https://www.upwork.com/freelancers/~010554d3d91f1f3997" ref='noreferer' target='_blank' className="btn-link">
+              <a href="https://www.upwork.com/freelancers/~010554d3d91f1f3997" rel="noopener noreferrer" target='_blank' className="btn-link">
                 Upwork
               </a>
               <span>/</span>
-              <a href="https://twitter.com/nathdhim" className="btn-link" target='_blank'  ref='noreferer' >
+              <a href="https://twitter.com/nathdhim" className="btn-link" target='_blank'  rel="noopener noreferrer" >
                 Twitter
               </a>
               <span>/</span>
-              <a href="https://dribbble.com/nathdhim" className="btn-link" target='_blank'  ref='noreferer' >
+              <a href="https://dribbble.com/nathdhim" className="btn-link" target='_blank'  rel="noopener noreferrer" >
                 Dribbble
               </a>
               <span>/</span>
-              <a href="https://www.linkedin.com/in/nathdhim/" className="btn-link" target='_blank'  ref='noreferer' >
+              <a href="https://www.linkedin.com/in/nathdhim/" className="btn-link" target='_blank'  rel="noopener noreferrer" >
                 Linkedin
               </a>
             </div>
