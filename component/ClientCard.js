6dc72e4dacd1390f71bc189f5ee92135666936a2
dhimas putra <hello.dhimasputra@gmail.com>
2022-09-16 21:52:52 +0800

    add video

--- component/ClientCard.js
+++ component/ClientCard.js
@@ -6,7 +6,7 @@ const ClientCard = (props) => {
     <Link href={props.page}><a className="client-card">
     <div className="video-container">
       <div className="btn-icon"><div className="icon i-arrow-right-up"></div></div>
-      <video autoPlay muted playsInline loop src={"/case.mp4"} ></video>
+      <video autoPlay muted playsInline loop src={"https://res.cloudinary.com/dtwh4nrmh/video/upload/v1663336335/case_r96ojv.mp4"} ></video>
     </div>
     <div className="client-detail">
       <p className="desc client-name">{props.clientname}</p>
