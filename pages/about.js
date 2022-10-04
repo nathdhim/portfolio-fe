c60a49c1ea765bd326fb5299915c9e517bcbc125
dhimas putra <hello.dhimasputra@gmail.com>
2022-10-04 13:31:18 +0800

    add bio

--- pages/about.js
+++ pages/about.js
@@ -31,32 +31,49 @@ export default function about() {
             </div>
           </div>
           <div className="text-container">
-            <p className="titled desc">About</p>
+            <p className="titled desc">Experience</p>
             <div className="content">
-              <h2>
-                Ut voluptatem fugiat eos officiis tempora explicabo non illo
-                possimus. Hic qui placeat quia sit et dolores expedita fugiat.
-                Impedit ut non ex. Qui eaque at. Sit voluptatem nam maxime.
-              </h2>
-              <h2>
-                Ut voluptatem fugiat eos officiis tempora explicabo non illo
-                possimus. Hic qui placeat quia sit et dolores expedita fugiat.
-                Impedit ut non ex. Qui eaque at. Sit voluptatem nam maxime.
-              </h2>
+              <div className="work-card">
+                <h2>UIUX Designer @ Itsavirus</h2>
+                <p className="desc">Jul 2022 - Present</p>
+              </div>
+              <div className="work-card">
+                <h2>Product Designer Freelancer</h2>
+                <p className="desc">Sep 2021 - Today</p>
+              </div>
+              <div className="work-card">
+                <h2>UIUX Designer & Illustrator @ Boxylabs</h2>
+                <p className="desc">Okt 2021 - Feb 2022</p>
+              </div>
+              <div className="work-card">
+                <h2>UIUX Designer Intern @ The Puzzlers</h2>
+                <p className="desc">Jul 2021 - Sep 2021</p>
+              </div>
             </div>
           </div>
           <div className="text-container">
-            <p className="titled desc">About</p>
+            <p className="titled desc">Work With</p>
             <div className="content">
               <h2>
-                Ut voluptatem fugiat eos officiis tempora explicabo non illo
-                possimus. Hic qui placeat quia sit et dolores expedita fugiat.
-                Impedit ut non ex. Qui eaque at. Sit voluptatem nam maxime.
+              Mijn Sollicitatie
               </h2>
               <h2>
-                Ut voluptatem fugiat eos officiis tempora explicabo non illo
-                possimus. Hic qui placeat quia sit et dolores expedita fugiat.
-                Impedit ut non ex. Qui eaque at. Sit voluptatem nam maxime.
+              Artreon
+              </h2>
+              <h2>
+              Joov
+              </h2>
+              <h2>
+              Wagerwith
+              </h2>
+              <h2>
+              Quibee
+              </h2>
+              <h2>
+              Healta Apps
+              </h2>
+              <h2>
+              Ario AI
               </h2>
             </div>
           </div>
