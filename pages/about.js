eab09ec371b26af91f4eced08a1ada04641f6120
dhimas putra <hello.dhimasputra@gmail.com>
2022-10-04 14:30:25 +0800

    add config

--- pages/about.js
+++ pages/about.js
@@ -1,27 +1,40 @@
 import Layout from "../component/Layout";
 import Image from "next/future/image";
+import { motion } from "framer-motion";
 
 export default function about() {
   return (
     <Layout>
       <section className="about-hero">
         <div className="content-container">
-          <motion.div className="hero-img"
-          animate={{ width: ["0%", "100%"] }}
-          transition={{
-            ease: [0.75, -0.01, 0.34, 1],
-            duration: 1,
-            delay: 0.5,
-          }}><motion.div className="img-wrapper">
-          <Image
-            src={
-              "https://res.cloudinary.com/dtwh4nrmh/image/upload/v1663336177/samples/cloudinary-group.jpg"
-            }
-            className="img"
-            fill
-          />
-        </motion.div></motion.div>
-          
+          <motion.div
+            className="hero-img"
+            animate={{ height: ["0", "20em"] }}
+            transition={{
+              ease: [0.75, -0.01, 0.34, 1],
+              duration: 1,
+              delay: 0.5,
+            }}
+          >
+            <motion.div
+              className="img-wrapper"
+              animate={{ scale: [1.35, 1] }}
+              transition={{
+                ease: [0.75, -0.01, 0.34, 1],
+                duration: 1,
+                delay: 0.5,
+              }}
+            >
+              <Image
+                src={
+                  "https://res.cloudinary.com/dtwh4nrmh/image/upload/v1664864703/Rectangle_1_cooxah.png"
+                }
+                className="img"
+                fill
+              />
+            </motion.div>
+          </motion.div>
+
           <div className="text-container">
             <p className="titled desc">About</p>
             <div className="content">
@@ -61,27 +74,13 @@ export default function about() {
           <div className="text-container">
             <p className="titled desc">Work With</p>
             <div className="content">
-              <h2>
-              Mijn Sollicitatie
-              </h2>
-              <h2>
-              Artreon
-              </h2>
-              <h2>
-              Joov
-              </h2>
-              <h2>
-              Wagerwith
-              </h2>
-              <h2>
-              Quibee
-              </h2>
-              <h2>
-              Healta Apps
-              </h2>
-              <h2>
-              Ario AI
-              </h2>
+              <h2>Mijn Sollicitatie</h2>
+              <h2>Artreon</h2>
+              <h2>Joov</h2>
+              <h2>Wagerwith</h2>
+              <h2>Quibee</h2>
+              <h2>Healta Apps</h2>
+              <h2>Ario AI</h2>
             </div>
           </div>
         </div>
