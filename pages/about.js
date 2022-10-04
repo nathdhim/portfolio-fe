13b5a85ba690437cc61244c7f96d5dfa445b0d10
dhimas putra <hello.dhimasputra@gmail.com>
2022-10-04 13:06:16 +0800

    change web concept

--- pages/about.js
+++ pages/about.js
@@ -1,30 +1,64 @@
 import Layout from "../component/Layout";
+import Image from "next/future/image";
 
 export default function about() {
   return (
     <Layout>
       <section className="about-hero">
         <div className="content-container">
-          <div className="hero-img"></div>
-          <div className="hero-text">
-            <h2>Hi, I’m Dhimas Putra</h2>
-            <p className="desc">
-              Ut voluptatem fugiat eos officiis tempora explicabo non illo
-              possimus. Hic qui placeat quia sit et dolores expedita fugiat.
-              Impedit ut non ex. Qui eaque at.
-            </p>
-            <p className="desc">
-              Sit voluptatem nam maxime. Veniam impedit dignissimos voluptatem
-              non fugit sint et iure. Aut alias rerum vel. Laudantium ea nostrum
-              voluptas dicta maiores quisquam veniam. Qui nostrum minus itaque
-              exercitationem et rerum nostrum ea corrupti. Fuga rerum dolor et
-              id. Sed voluptatem neque omnis soluta amet est et. Esse est aut
-              aut quo.
-            </p>
-            <p className="desc">
-              Fuga natus sed ut omnis sunt quidem dolores corporis. Qui
-              voluptatem voluptates. Voluptatem eius repudiandae.
-            </p>
+          <div className="hero-img">
+            <Image
+              src={
+                "https://res.cloudinary.com/dtwh4nrmh/image/upload/v1663336177/samples/cloudinary-group.jpg"
+              }
+              className="img"
+              fill
+            />
+          </div>
+          <div className="text-container">
+            <p className="titled desc">About</p>
+            <div className="content">
+              <h2>
+                Ut voluptatem fugiat eos officiis tempora explicabo non illo
+                possimus. Hic qui placeat quia sit et dolores expedita fugiat.
+                Impedit ut non ex. Qui eaque at. Sit voluptatem nam maxime.
+              </h2>
+              <h2>
+                Ut voluptatem fugiat eos officiis tempora explicabo non illo
+                possimus. Hic qui placeat quia sit et dolores expedita fugiat.
+                Impedit ut non ex. Qui eaque at. Sit voluptatem nam maxime.
+              </h2>
+            </div>
+          </div>
+          <div className="text-container">
+            <p className="titled desc">About</p>
+            <div className="content">
+              <h2>
+                Ut voluptatem fugiat eos officiis tempora explicabo non illo
+                possimus. Hic qui placeat quia sit et dolores expedita fugiat.
+                Impedit ut non ex. Qui eaque at. Sit voluptatem nam maxime.
+              </h2>
+              <h2>
+                Ut voluptatem fugiat eos officiis tempora explicabo non illo
+                possimus. Hic qui placeat quia sit et dolores expedita fugiat.
+                Impedit ut non ex. Qui eaque at. Sit voluptatem nam maxime.
+              </h2>
+            </div>
+          </div>
+          <div className="text-container">
+            <p className="titled desc">About</p>
+            <div className="content">
+              <h2>
+                Ut voluptatem fugiat eos officiis tempora explicabo non illo
+                possimus. Hic qui placeat quia sit et dolores expedita fugiat.
+                Impedit ut non ex. Qui eaque at. Sit voluptatem nam maxime.
+              </h2>
+              <h2>
+                Ut voluptatem fugiat eos officiis tempora explicabo non illo
+                possimus. Hic qui placeat quia sit et dolores expedita fugiat.
+                Impedit ut non ex. Qui eaque at. Sit voluptatem nam maxime.
+              </h2>
+            </div>
           </div>
         </div>
       </section>
